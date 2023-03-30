a67a6d148d2fb0e681f811e412d4bd56cd7cc9d1
ljhmd00 <lmsmd00@naver.com>
2023-03-30 16:44:40 +0900

    0330 commit

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,76 @@ function App() {
                     </ul>
                 </div>
             </header>
+            <main>
+                {/* Introduce */}
+                <div className="bg-green-100 max-w-screen-xl mx-auto h-96 flex justify-center items-center">
+                    소개하는 내용
+                </div>
+                <ul className="bg-indigo-100 max-w-screen-xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center gap-8 py-8">
+                    <li className="bg-purple-200 w-60 h-72 rounded-xl">
+                        <div className="bg-yellow-100 w-full h-1/2 object-cover rounded-t-xl">
+                            프로젝트 이미지
+                        </div>
+                        <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+                        <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+                    </li>
+                    <li className="bg-purple-200 w-60 h-72 rounded-xl">
+                        <div className="bg-yellow-100 w-full h-1/2 object-cover rounded-t-xl">
+                            프로젝트 이미지
+                        </div>
+                        <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+                        <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+                    </li>
+                    <li className="bg-purple-200 w-60 h-72 rounded-xl">
+                        <div className="bg-yellow-100 w-full h-1/2 object-cover rounded-t-xl">
+                            프로젝트 이미지
+                        </div>
+                        <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+                        <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+                    </li>
+                    <li className="bg-purple-200 w-60 h-72 rounded-xl">
+                        <div className="bg-yellow-100 w-full h-1/2 object-cover rounded-t-xl">
+                            프로젝트 이미지
+                        </div>
+                        <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+                        <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+                    </li>
+                    <li className="bg-purple-200 w-60 h-72 rounded-xl">
+                        <div className="bg-yellow-100 w-full h-1/2 object-cover rounded-t-xl">
+                            프로젝트 이미지
+                        </div>
+                        <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+                        <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+                    </li>
+                    <li className="bg-purple-200 w-60 h-72 rounded-xl">
+                        <div className="bg-yellow-100 w-full h-1/2 object-cover rounded-t-xl">
+                            프로젝트 이미지
+                        </div>
+                        <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+                        <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+                    </li>
+                    <li className="bg-purple-200 w-60 h-72 rounded-xl">
+                        <div className="bg-yellow-100 w-full h-1/2 object-cover rounded-t-xl">
+                            프로젝트 이미지
+                        </div>
+                        <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+                        <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+                    </li>
+                    <li className="bg-purple-200 w-60 h-72 rounded-xl">
+                        <div className="bg-yellow-100 w-full h-1/2 object-cover rounded-t-xl">
+                            프로젝트 이미지
+                        </div>
+                        <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
+                        <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+                    </li>
+                </ul>
+                <div className="bg-gray-100 max-w-screen-xl mx-auto h-96 flex justify-center items-center">
+                    내연락처
+                </div>
+            </main>
+            <footer className="max-x-screen-xl mx-auto flex justify-end py-1 md:py-2">
+                2023. Designed by, lee
+            </footer>
         </div>
     );
 }

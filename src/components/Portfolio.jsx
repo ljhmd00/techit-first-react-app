f987dc6b43a6712ae9d72569119a317e53608087
ljhmd00 <lmsmd00@naver.com>
2023-04-03 17:00:04 +0900

    portfolioCard map추가

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,16 +1,19 @@
 import PortfolioCard from "./PortfolioCard";
+import projectData from "../projectData.json";
 
 const Portfolio = () => {
     return (
         <ul className="bg-indigo-100 max-w-screen-xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center gap-8 py-8">
-            <PortfolioCard />
-            <PortfolioCard />
-            <PortfolioCard />
-            <PortfolioCard />
-            <PortfolioCard />
-            <PortfolioCard />
-            <PortfolioCard />
-            <PortfolioCard />
+            {projectData.map((v, i) => {
+                return (
+                    <PortfolioCard
+                        key={i}
+                        projectImg={v.projectImg}
+                        projectTitle={v.projectTitle}
+                        projectContents={v.projectContents}
+                    />
+                );
+            })}
         </ul>
     );
 };
--- src/components/PortfolioCard.jsx
+++ src/components/PortfolioCard.jsx
@@ -1,11 +1,11 @@
-const PortfolioCard = () => {
+const PortfolioCard = (props) => {
     return (
         <li className="bg-purple-200 w-60 h-72 rounded-xl">
             <div className="bg-yellow-100 w-full h-1/2 object-cover rounded-t-xl">
-                프로젝트 이미지
+                {props.projectImg}
             </div>
-            <div className="text-xl font-bold mt-2 mx-2">프로젝트 제목</div>
-            <div className="mt-2 mx-2">프로젝트 간략한 설명</div>
+            <div className="text-xl font-bold mt-2 mx-2">{props.projectTitle}</div>
+            <div className="mt-2 mx-2">{props.projectContents}</div>
         </li>
     );
 };

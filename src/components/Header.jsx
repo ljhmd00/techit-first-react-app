498879e11c75fb6aaf5dd56ea134c24bd7605947
ljhmd00 <lmsmd00@naver.com>
2023-04-04 00:50:01 +0900

    link 설정

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,11 +1,12 @@
+import { Link } from "react-router-dom";
+
 const Header = () => {
     return (
         <header className="bg-blue-100 h-12 md:h-20">
             <div className="max-w-screen-xl mx-auto h-full flex justify-between items-center">
-                <div>juhyeok's Portfolio</div>
-                {/* <div>
-                        <img className="w-20" src={youtube} alt="youtube" />
-                    </div> */}
+                <Link to="/">
+                    <div>juhyeok's Portfolio</div>
+                </Link>
                 <ul className="flex gap-2 md:gap-8 text-xs sm:text-sm md:text-base">
                     <li className="bg-indigo-400 btn-style">Introduce</li>
                     <li className="bg-purple-400 btn-style">Portfolio</li>

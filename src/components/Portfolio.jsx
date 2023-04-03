ec1744cddce5cc5596ab5cabd65a9e49b4fc6df7
ljhmd00 <lmsmd00@naver.com>
2023-04-04 00:50:18 +0900

    add props

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -1,16 +1,16 @@
 import PortfolioCard from "./PortfolioCard";
-import projectData from "../projectData.json";
+import projectDate from "../projectData.json";
 
 const Portfolio = () => {
     return (
         <ul className="bg-indigo-100 max-w-screen-xl mx-auto grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 justify-items-center gap-8 py-8">
-            {projectData.map((v, i) => {
+            {projectDate.map((v) => {
                 return (
                     <PortfolioCard
-                        key={i}
                         projectImg={v.projectImg}
                         projectTitle={v.projectTitle}
                         projectContents={v.projectContents}
+                        projectNum={v.projectNum}
                     />
                 );
             })}
